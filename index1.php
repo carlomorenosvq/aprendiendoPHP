f71cfdfc42f9768b9ac7662b3f7af453b22ad165
carlosmorenosvq <torresmorenojosecarlos@gmail.com>
2023-03-22 12:53:36 +0100

    first commit

---
+++ index1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<!-- <body> -->
+<!-- <form action="index2.php" method="get">
+        <h1>Introduce tu nombre</h1><input type="text" name="nombre" id="nombre">
+        <input type="submit" value="Enviar">
+        <h1>Introduce tu edad</h1><input type="text" name="edad" id="edad">
+        <input type="submit" value="Enviar">
+    </form> -->
+<!-- <a href="index2.php?nombre=Carlos">Enviame esta</a> -->
+<!-- </body> -->
+
+
+<body>
+    <!-- <form action="index2.php" method="get">
+
+        <h1>Tu pareja ha aumentado sus gastos de vestuario</h1>
+        Verdadero<input type="radio" name="p1" value="Verdadero">
+        Falso<input type="radio" name="p1" value="Falso">
+
+        <h1>Frecuentemente se olvida de donde ha estado</h1>
+        Verdadero<input type="radio" name="p2" value="Verdadero">
+        Falso<input type="radio" name="p2" value="Falso">
+
+
+
+
+        <input type="submit" value="Enviar">
+    </form> -->
+
+
+    
+</body> 
+
+
+<!-- // Realiza un pograma que pida un día de la semana y te devuelva el número del día
+
+
+
+//Realiza un pograma que nos diga si hay una probabilidad de que nuestra pareja nos sea infiel.
+El programa irá haciendo preguntas que el usuario responderá como verdadero o falso.
+Cada pregunta verdadera suma 3 puntos, las falsas no suman. Podemos hacerlo con radio button
+.
+1.  -->
+
+<!-- 
+Realiza un pograma que nos diga el horoscopo a partir del día y mes de nacimiento -->
+
+
+
+
+</html>
