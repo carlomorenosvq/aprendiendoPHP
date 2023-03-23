6e2009582d51c69ba088d278597be44aaa380118
carlosmorenosvq <torresmorenojosecarlos@gmail.com>
2023-03-23 11:15:44 +0100

    mas cozas de php

--- index1.php
+++ index1.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
+</head> -->
 
 <!-- <body> -->
 <!-- <form action="index2.php" method="get">
@@ -19,8 +19,8 @@
 <!-- </body> -->
 
 
-<body>
-    <!-- <form action="index2.php" method="get">
+
+<!-- <form action="index2.php" method="get">
 
         <h1>Tu pareja ha aumentado sus gastos de vestuario</h1>
         Verdadero<input type="radio" name="p1" value="Verdadero">
@@ -37,8 +37,8 @@
     </form> -->
 
 
-    
-</body> 
+
+
 
 
 <!-- // Realiza un pograma que pida un día de la semana y te devuelva el número del día
@@ -55,6 +55,89 @@ Cada pregunta verdadera suma 3 puntos, las falsas no suman. Podemos hacerlo con 
 Realiza un pograma que nos diga el horoscopo a partir del día y mes de nacimiento -->
 
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<!-- <style>
+    body {
+        background-color: gray;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .contenedor {
+        width: 500px;
+        padding: 10px;
+        border: 5px solid white;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .cajita {
+        margin: 10px 0px;
+        width: 140px;
+        height: 140px;
+
+        background-color: black;
+        cursor: pointer;
+        margin: 10px 0;
+    }
+</style>
+<body>
+
+    <div class="contenedor">
+        <a href="index2.php?img=1"><div class="cajita"> </div></a>
+        <a href="index2.php?img=2"><div class="cajita"> </div></a>
+        <a href="index2.php?img=3"><div class="cajita"> </div></a>
+        <a href="index2.php?img=4"><div class="cajita"> </div></a>
+        <a href="index2.php?img=5"><div class="cajita"> </div></a>
+        <a href="index2.php?img=6"><div class="cajita"> </div></a>
+        <a href="index2.php?img=7"><div class="cajita"> </div></a>
+        <a href="index2.php?img=8"><div class="cajita"> </div></a>
+        <a href="index2.php?img=9"><div class="cajita"> </div></a>
+        
+    </div> -->
+
+
+<!-- Programa de control de acceso a una caja fuerte. La combinacion será un numero de 4 cifras, y el programa nos pedirá la combinacion para abrirla -->
+
+<?php
+if (isset($_GET['intentos'])) {
+    $_GET['intentos']--;
+    $intentos = $_GET['intentos'];
+} else {
+    $intentos = 4;
+}
+
+if ($intentos == 0) {
+    echo "<p>Te has quedado sin intentos. Se cierra la caja fuerte";
+} else if ($_GET['contra'] == 1234){
+    echo "<p>Oleeeeeeeee vaya maquinon</p>";
+}
+
+
+
+
+?>
+    <p>Introduce la clave</p>
+
+    <form action="index1.php" method="get">
+        <input type="text" name="contra" id="">
+        <input type="hidden" name="intentos" value="<?= $intentos ?>">
+        <input type="submit" value="Probar">
+    </form>
+<?php  ?>
+</body>
 
 </html>
--- index2.php
+++ index2.php
@@ -7,28 +7,88 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    body {
+        background-color: gray;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .contenedor {
+        width: 500px;
+        padding: 10px;
+        border: 5px solid white;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .cajita {
+        margin: 10px 0px;
+        width: 140px;
+        height: 140px;
+
+        background-color: black;
+        cursor: pointer;
+        margin: 10px 0;
+    }
+</style>
 
 <body>
-    <?php
-
-    $suma = 0;
-    
-    if($_GET['p1'] == "Verdadero"){
-        $suma = $suma+3;
-    }
-
-    if($_GET['p2'] == "Verdadero"){
-        $suma = $suma+3;
-    }
-
-    echo $suma;
 
 
-
-   
+<div class="contenedor">
+    <?php 
+        if ($_GET['img'] == 1) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
+        if ($_GET['img'] == 2) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
+        if ($_GET['img'] == 3) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
+        if ($_GET['img'] == 4) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
+        if ($_GET['img'] == 5) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
+        if ($_GET['img'] == 6) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
+        if ($_GET['img'] == 7) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
+        if ($_GET['img'] == 8) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
+        if ($_GET['img'] == 9) {
+            echo '<div class="cajita" style="background-image:url(img/' . $_GET['img'] . '.jpg)"></div>';
+        }else{
+            echo '<div class="cajita"> </div>';
+        }
     ?>
-
-
+</div>
 
 
 
